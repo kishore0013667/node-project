27b029477b193aaddc131853f3409169a7cd2891
Kishore M <142767995+kishore0013667@users.noreply.github.com>
2026-04-27 14:27:31 +0530

    Add product routes and initialize Day 9 project

---
+++ day 9/app/app.js
@@ -0,0 +1,16 @@
+const user = {};
+
+user.name = "Kishore";
+user.mobile = "9876543210";
+user.email = "kishore@email.com";
+
+user.skills = ["HTML", "CSS", "JS"];
+
+user.NewData = {
+  name: "React"
+};
+
+user.skills.push("NodeJS");
+user.NewData.version = "18.0";
+
+console.log(user);
