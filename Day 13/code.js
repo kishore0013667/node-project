62db26b6f19d98d38695da6f4f9a2461714436de
Kishore M <142767995+kishore0013667@users.noreply.github.com>
2026-04-28 14:14:31 +0530

    Add Day 13 files (server, fetch, react practice)

---
+++ Day 13/code.js
@@ -0,0 +1,9 @@
+// map
+let arr = [1,2,3];
+console.log(arr.map(x => x * 2));
+
+// filter
+console.log(arr.filter(x => x > 1));
+
+// find
+console.log(arr.find(x => x === 2));
---
+++ Day 13/fetch.js
@@ -0,0 +1,6 @@
+async function getUsers() {
+  let res = await fetch("https://jsonplaceholder.typicode.com/users");
+  let data = await res.json();
+  console.log(data);
+}
+getUsers();
